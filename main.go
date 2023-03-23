e04ae9e46cb48843519d39b38850d177ff13aaa9
calgor <ahomayoni684@gmail.com>
2023-03-23 10:08:42 +0330

    fixing the hash problem

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/Calgorr/URL_Shortener/handle"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/:hash", handle.Redirect)
 	e.POST("/new", handle.SaveUrl)
+	e.Use(middleware.Logger())
 }
